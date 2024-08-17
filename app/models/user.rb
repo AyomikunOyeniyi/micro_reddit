775a16506cec7aaf2d3e22ad43054436f7de7a27
Ayomikun Oyeniyi <gideonoye1@gmail.com>
2024-08-17 00:54:19 -0400

    Generate User model and add validations

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  validates :username,  presence: true, length: { minimum: 7 }, uniqueness: true
+  validates :email,  presence: true, format: { with: /\A\S+@\S+\Z/, message: "must be valid email address" }
+end
