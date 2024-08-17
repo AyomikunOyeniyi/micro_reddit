c9f001004c261a5552838612bbf5f4fd7f089a63
Ayomikun Oyeniyi <gideonoye1@gmail.com>
2024-08-17 02:30:40 -0400

    Write and check associations between posts and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :username,  presence: true, length: { minimum: 7 }, uniqueness: true
   validates :email,  presence: true, format: { with: /\A\S+@\S+\Z/, message: "must be valid email address" }
+
+  has_many :posts
 end
